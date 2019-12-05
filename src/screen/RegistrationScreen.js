7e3d1ef6e7b758db0206113f2cb77ce788170299
kirill_lalala <mr.ilikov@gmail.com>
2019-12-05 20:17:43 +0300

    refactoring Registration component

--- src/screen/RegistrationScreen.js
+++ src/screen/RegistrationScreen.js
@@ -57,28 +57,41 @@ const RegistrationScreen = props => {
     <View style={styles.container}>
       <View style={styles.photoBlock}>
         <Text style={styles.photoText}>Фото</Text>
-        <TouchableOpacity
-          style={styles.avatarPlaceholder}
-          onPress={handlePickAvatar}>
-          <Image
-            source={{uri: avatar ? avatar.uri : avatar}}
-            style={styles.avatar}
-          />
-          {!avatar && (
-            <Svg
-              width="26"
-              height="26"
-              viewBox="0 0 26 26"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg">
-              <Path
-                d="M23.1111 0H2.88891C1.29276 0 0 1.29276 0 2.88891V23.1112C0 24.7072 1.29276 26 2.88891 26H23.1112C24.7072 26 26 24.7072 26 23.1111V2.88891C26 1.29276 24.7072 0 23.1111 0ZM13 4.33333C15.3906 4.33333 17.3333 6.27609 17.3333 8.66667C17.3333 11.0644 15.3906 13 13 13C10.6094 13 8.66667 11.0644 8.66667 8.66667C8.66667 6.27609 10.6094 4.33333 13 4.33333ZM21.6667 21.6667H4.33333V20.2222C4.33333 17.3333 10.1111 15.7445 13 15.7445C15.8889 15.7445 21.6667 17.3333 21.6667 20.2222V21.6667Z"
-                fill="#3066E0"
-                fill-opacity="0.37"
-              />
-            </Svg>
+        <View>
+          <TouchableOpacity
+            style={styles.avatarPlaceholder}
+            onPress={handlePickAvatar}>
+            <Image
+              source={{uri: avatar ? avatar.uri : avatar}}
+              style={styles.avatar}
+            />
+            {!avatar && (
+              <Svg
+                width="26"
+                height="26"
+                viewBox="0 0 26 26"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <Path
+                  d="M23.1111 0H2.88891C1.29276 0 0 1.29276 0 2.88891V23.1112C0 24.7072 1.29276 26 2.88891 26H23.1112C24.7072 26 26 24.7072 26 23.1111V2.88891C26 1.29276 24.7072 0 23.1111 0ZM13 4.33333C15.3906 4.33333 17.3333 6.27609 17.3333 8.66667C17.3333 11.0644 15.3906 13 13 13C10.6094 13 8.66667 11.0644 8.66667 8.66667C8.66667 6.27609 10.6094 4.33333 13 4.33333ZM21.6667 21.6667H4.33333V20.2222C4.33333 17.3333 10.1111 15.7445 13 15.7445C15.8889 15.7445 21.6667 17.3333 21.6667 20.2222V21.6667Z"
+                  fill="#3066E0"
+                  fill-opacity="0.37"
+                />
+              </Svg>
+            )}
+          </TouchableOpacity>
+
+          {avatar && (
+            <TouchableOpacity
+              style={styles.deletePhoto}
+              onPress={() => setAvatar(null)}>
+              <View>
+                <View style={[styles.close, styles.closeRotate45]} />
+                <View style={[styles.close, styles.closeRotate275]} />
+              </View>
+            </TouchableOpacity>
           )}
-        </TouchableOpacity>
+        </View>
       </View>
 
       <Inputs
@@ -88,6 +101,7 @@ const RegistrationScreen = props => {
         updatePassword={updatePassword}
         email={email}
         password={password}
+        isDisable={isDisableElement}
       />
       <View style={styles.policyBlock}>
         <Checkbox style={styles.checkBox} />
@@ -98,7 +112,11 @@ const RegistrationScreen = props => {
       </View>
 
       <View style={styles.regButton}>
-        <Button text="Регистрация" handle={handleSignUp} disable={isDisableElement} />
+        <Button
+          text="Регистрация"
+          handle={handleSignUp}
+          disable={isDisableElement}
+        />
       </View>
     </View>
   );
@@ -159,6 +177,36 @@ const styles = StyleSheet.create({
   regButton: {
     alignItems: 'center',
     marginTop: 40,
+  },
+  deletePhoto: {
+    position: 'absolute',
+    right: -15,
+    width: 21,
+    height: 21,
+    borderRadius: 50,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.28,
+    shadowRadius: 6,
+    elevation: 5,
+  },
+  close: {
+    position: 'absolute',
+    top: 10,
+    left: 6,
+    width: 10,
+    height: 1,
+    backgroundColor: '#CA2121',
+  },
+  closeRotate45: {
+    transform: [{rotate: '45deg'}],
+  },
+  closeRotate275: {
+    transform: [{rotate: '-45deg'}],
   },
 });
 
