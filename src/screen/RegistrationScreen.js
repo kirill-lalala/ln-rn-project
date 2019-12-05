68dcbb6b50d51e474c336640afe546701f0f8d78
kirill_lalala <mr.ilikov@gmail.com>
2019-12-05 11:19:25 +0300

    styles avatar

--- src/screen/RegistrationScreen.js
+++ src/screen/RegistrationScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import Checkbox from 'react-native-custom-checkbox';
 import Svg, {Path} from 'react-native-svg';
 import Inputs from '../components/Inputs';
@@ -12,6 +12,10 @@ const RegistrationScreen = props => {
   const [errorLoginMessage, setErrorLoginMessage] = useState(' ');
   const [errorPasswordMessage, setErrorPasswordMessage] = useState(' ');
   const [avatar, setAvatar] = useState(null);
+
+  const handlePickAvatar = async () => {
+
+  };
 
   const updateLogin = email => setEmail(email);
   const updatePassword = password => setPassword(password);
@@ -43,7 +47,8 @@ const RegistrationScreen = props => {
     <View style={styles.container}>
       <View style={styles.photoBlock}>
         <Text style={styles.photoText}>Фото</Text>
-        <View style={styles.photo}>
+        <TouchableOpacity style={styles.avatarPlaceholder} onPress={handlePickAvatar}>
+          <Image source={{uri: avatar}} style={styles.avatar} />
           <Svg
             width="26"
             height="26"
@@ -52,10 +57,11 @@ const RegistrationScreen = props => {
             xmlns="http://www.w3.org/2000/svg">
             <Path
               d="M23.1111 0H2.88891C1.29276 0 0 1.29276 0 2.88891V23.1112C0 24.7072 1.29276 26 2.88891 26H23.1112C24.7072 26 26 24.7072 26 23.1111V2.88891C26 1.29276 24.7072 0 23.1111 0ZM13 4.33333C15.3906 4.33333 17.3333 6.27609 17.3333 8.66667C17.3333 11.0644 15.3906 13 13 13C10.6094 13 8.66667 11.0644 8.66667 8.66667C8.66667 6.27609 10.6094 4.33333 13 4.33333ZM21.6667 21.6667H4.33333V20.2222C4.33333 17.3333 10.1111 15.7445 13 15.7445C15.8889 15.7445 21.6667 17.3333 21.6667 20.2222V21.6667Z"
-              fill="#A3B7E4"
+              fill="#3066E0"
+              fill-opacity="0.37"
             />
           </Svg>
-        </View>
+        </TouchableOpacity>
       </View>
 
       <Inputs
@@ -98,13 +104,20 @@ const styles = StyleSheet.create({
     opacity: 0.5,
     fontSize: 13,
   },
-  photo: {
+  avatarPlaceholder: {
+    position: 'relative',
     width: 100,
     height: 100,
     backgroundColor: '#E7E7E7',
     borderRadius: 50,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  avatar: {
+    position: 'absolute',
+    width: 100,
+    height: 100,
+    borderRadius: 50,
   },
   policyBlock: {
     flexDirection: 'row',
