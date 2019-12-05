ccc635b97f0585132495f4fbb7592d9a9ea126bf
kirill_lalala <mr.ilikov@gmail.com>
2019-12-05 12:46:59 +0300

    load avatar

--- src/screen/RegistrationScreen.js
+++ src/screen/RegistrationScreen.js
@@ -5,6 +5,7 @@ import Svg, {Path} from 'react-native-svg';
 import Inputs from '../components/Inputs';
 import Button from '../components/Button';
 import fire from '../config/fire';
+import ImagePicker from 'react-native-image-picker';
 
 const RegistrationScreen = props => {
   const [email, setEmail] = useState('');
@@ -13,8 +14,13 @@ const RegistrationScreen = props => {
   const [errorPasswordMessage, setErrorPasswordMessage] = useState(' ');
   const [avatar, setAvatar] = useState(null);
 
-  const handlePickAvatar = async () => {
-
+  const handlePickAvatar = () => {
+    const options = {
+      noData: true,
+    };
+    ImagePicker.launchImageLibrary(options, response => {
+      response.uri && setAvatar(response);
+    });
   };
 
   const updateLogin = email => setEmail(email);
@@ -47,20 +53,27 @@ const RegistrationScreen = props => {
     <View style={styles.container}>
       <View style={styles.photoBlock}>
         <Text style={styles.photoText}>Фото</Text>
-        <TouchableOpacity style={styles.avatarPlaceholder} onPress={handlePickAvatar}>
-          <Image source={{uri: avatar}} style={styles.avatar} />
-          <Svg
-            width="26"
-            height="26"
-            viewBox="0 0 26 26"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <Path
-              d="M23.1111 0H2.88891C1.29276 0 0 1.29276 0 2.88891V23.1112C0 24.7072 1.29276 26 2.88891 26H23.1112C24.7072 26 26 24.7072 26 23.1111V2.88891C26 1.29276 24.7072 0 23.1111 0ZM13 4.33333C15.3906 4.33333 17.3333 6.27609 17.3333 8.66667C17.3333 11.0644 15.3906 13 13 13C10.6094 13 8.66667 11.0644 8.66667 8.66667C8.66667 6.27609 10.6094 4.33333 13 4.33333ZM21.6667 21.6667H4.33333V20.2222C4.33333 17.3333 10.1111 15.7445 13 15.7445C15.8889 15.7445 21.6667 17.3333 21.6667 20.2222V21.6667Z"
-              fill="#3066E0"
-              fill-opacity="0.37"
-            />
-          </Svg>
+        <TouchableOpacity
+          style={styles.avatarPlaceholder}
+          onPress={handlePickAvatar}>
+          <Image
+            source={{uri: avatar ? avatar.uri : avatar}}
+            style={styles.avatar}
+          />
+          {!avatar && (
+            <Svg
+              width="26"
+              height="26"
+              viewBox="0 0 26 26"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <Path
+                d="M23.1111 0H2.88891C1.29276 0 0 1.29276 0 2.88891V23.1112C0 24.7072 1.29276 26 2.88891 26H23.1112C24.7072 26 26 24.7072 26 23.1111V2.88891C26 1.29276 24.7072 0 23.1111 0ZM13 4.33333C15.3906 4.33333 17.3333 6.27609 17.3333 8.66667C17.3333 11.0644 15.3906 13 13 13C10.6094 13 8.66667 11.0644 8.66667 8.66667C8.66667 6.27609 10.6094 4.33333 13 4.33333ZM21.6667 21.6667H4.33333V20.2222C4.33333 17.3333 10.1111 15.7445 13 15.7445C15.8889 15.7445 21.6667 17.3333 21.6667 20.2222V21.6667Z"
+                fill="#3066E0"
+                fill-opacity="0.37"
+              />
+            </Svg>
+          )}
         </TouchableOpacity>
       </View>
 
